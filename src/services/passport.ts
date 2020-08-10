3ff080fe90a056885037881f31b7e2184684bfc8
Eliezer Soares <eliezerdrum@gmail.com>
2020-08-10 17:26:14 -0300

    Web Application

--- src/services/passport.ts
+++ src/services/passport.ts
@@ -4,7 +4,7 @@ const token = new snoowrap({
   userAgent: 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36',
   clientId: 'xvU7rKcS9vHe6g',
   clientSecret: 'tp3DY5521MkReIS-JCrMpzFfibE',
-  refreshToken: '572816785640-D-jEGvwANBRkdAmjQ_mw5n7lSpA',
+  refreshToken: '572816785640-M4DnPDAu0b9KAanWFUkAEwijL8M',
 });
 
 export default token;
